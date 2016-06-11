b97fce65b53f87ff90c85787cf78766aa7473c5f
Alexey Khomych <alexeyfurious@gmail.com>
2016-06-11 23:23:26 +0300

    added methods of CarWash

--- ObjectiveC/ObjectiveC/Source/AKICarWash/AKICarWash.h
+++ ObjectiveC/ObjectiveC/Source/AKICarWash/AKICarWash.h
@@ -9,7 +9,21 @@
 #import <Foundation/Foundation.h>
 
 #import "AKIBuilding.h"
+#import "AKIWorker.h"
 
-@interface AKICarWash : AKIBuilding
+@interface AKICarWash : NSObject
+@property (nonatomic, readonly, assign) NSArray *boxs;
+@property (nonatomic, readonly, assign) NSArray *workers;
+@property (nonatomic, readonly, assign) NSArray *cars;
+@property (nonatomic, assign)           NSUInteger workerCount;
+@property (nonatomic, assign)           NSUInteger carCount;
+
++ (instancetype)carWash;
+- (void)addCar:(id)car;
+- (void)removeCar:(id)car;
+- (void)addWorker:(AKIWorker *)worker;
+- (void)removeWorker:(AKIWorker *)worker;
+- (AKIWorker *)getFreeWorker;
+- (AKIBuilding *)getFreeBox;
 
 @end
--- ObjectiveC/ObjectiveC/Source/AKICarWash/AKICarWash.m
+++ ObjectiveC/ObjectiveC/Source/AKICarWash/AKICarWash.m
@@ -7,7 +7,84 @@
 //
 
 #import "AKICarWash.h"
+#import "NSObject+AKICategory.h"
+
+@interface AKICarWash()
+@property (nonatomic, assign) NSMutableArray *_boxs;
+@property (nonatomic, assign) NSMutableArray *_workers;
+@property (nonatomic, assign) NSMutableArray *_cars;
+
+@end
 
 @implementation AKICarWash
+
+#pragma mark
+#pragma mark Init/dealloc
+
+- (void)dealloc {
+    self._boxs = nil;
+    self._workers = nil;
+    self._cars = nil;
+    
+    [super dealloc];
+}
+
++ (instancetype)carWash {
+    return [[[self alloc] init] autorelease];
+}
+
+#pragma mark -
+#pragma mark Accessors
+
+- (NSArray *)boxs {
+    return [[self._boxs copy] autorelease];
+}
+
+- (NSArray *)workers {
+    return [[self._workers copy] autorelease];
+}
+
+- (NSArray *)cars {
+    return [[self._cars copy] autorelease];
+}
+
+#pragma mark
+#pragma Public Implementations
+
+- (void)addCar:(id)car {
+    [self._cars addObject:car];
+}
+
+- (void)removeCar:(id)car {
+    [self._cars removeObject:car];
+}
+
+- (void)addWorker:(AKIWorker *)worker {
+    [self._workers addObject:worker];
+}
+
+- (void)removeWorker:(AKIWorker *)worker {
+    [self._workers removeObject:worker];
+}
+
+- (AKIWorker *)getFreeWorker {
+    for (AKIWorker *worker in self._workers) {
+        if ([worker isFree]) {
+            return worker;
+        }
+    }
+    
+    return nil;
+}
+
+- (AKIBuilding *)getFreeBox {
+    for (AKIBuilding *box in self._boxs) {
+        if ([box isFree]) {
+            return box;
+        }
+    }
+    
+    return nil;
+}
 
 @end
